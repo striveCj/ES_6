41472970197b0ad1bb432fdc2551d5ecb7700763
ChenJie <530216775@qq.com>
2019-08-15 21:11:52 +0800

    ES6 模块封装代码 导入导出

---
+++ js/example.js
@@ -0,0 +1,28 @@
+export var color="red";
+export let name="Nicholas";
+export  const magicNumber=7;
+
+export function  sum(num1,num2) {
+    return num1+num1;
+}
+
+export class Rectangle{
+    constructor(length,width) {
+        this.length=length;
+        this.width=width;
+    }
+
+}
+function subtract(num1,num2) {
+    return num1-num2;
+}
+
+export {subtract as add}
+export function multiply(num1,num2) {
+    return num1*num2;
+}
+
+export function setName(newName) {
+    name=newName;
+}
+
