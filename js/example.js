841877a955ded3b683ad0a15f1d25f5728400f1a
ChenJie <530216775@qq.com>
2019-08-16 20:00:58 +0800

    ES6 模块封装代码 导入导出

--- js/example.js
+++ js/example.js
@@ -26,3 +26,11 @@ export function setName(newName) {
     name=newName;
 }
 
+Array.prototype.pushAll=function (items) {
+    if (!Array.isArray(items)){
+        throw new TypeError("参数必须是一个数组");
+    }
+
+    return this.push(...items);
+}
+
